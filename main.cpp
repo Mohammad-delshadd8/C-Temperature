51be3d1a976863d7b3bb3ed07377ffe90b55ff9d
Mohammad Delshad <88682305+Mohammad-delshadd8@users.noreply.github.com>
2022-04-08 16:52:52 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    float DAMA;
+    cout << "dama ra be celcius vared konid" <<"    " ;
+    cin >> DAMA;
+
+    cout << "dama be Farenhite:"<< "    "<<  DAMA*1.8+32 ;
+    cout << "    "<<"dama be Kelvin:"<< "    "<<  DAMA+273 << endl;
+    return 0;
+}
